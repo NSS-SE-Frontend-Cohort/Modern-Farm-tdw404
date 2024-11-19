c8f2449f291816d7ecb94681af2aa3d1166dedd5
Tom Walsh <walsth@gmail.com>
2024-11-19 12:53:12 -0600

    Added seed modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,7 @@
-console.log("Welcome to the main module")
+import { createPlan } from "./plan.js"
+import { createSoybean } from "./seeds/soybean.js"
 
+
+console.log("Welcome to the main module")
+debugger 
+ const yearlyPlan = createPlan()
---
+++ src/scripts/seeds/corn.js
@@ -0,0 +1,12 @@
+export const createCorn = () => ([
+    {
+        type: "Corn",
+        height: 180,
+        output: 6
+    },
+    {
+        type: "Corn",
+        height: 180,
+        output: 6
+    }
+])
